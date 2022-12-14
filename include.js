f4c86d8e3cff3401029e8fb2d0aac4dec7d357ce
4109 <mattapallisravya@gmail.com>
2022-12-14 11:05:14 -0600

    Final Updates

--- include.js
+++ include.js
@@ -1,6 +1,7 @@
 
 function includeHTML() {
-    var z, i, elmnt, file, xhttp;
+    console.log("I am working")
+  var z, i, elmnt, file, xhttp;
     /* Loop through a collection of all HTML elements: */
     z = document.getElementsByTagName("*");
     for (i = 0; i < z.length; i++) {
